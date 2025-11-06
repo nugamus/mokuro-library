0f7e37d5885a356758c2f06da0b013c75aa6e501
nguyenston <phucnguyen1999.nn@gmail.com>
2025-11-05 22:27:07 -0500

    feat(frontend): define shared types

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,27 @@
+// --- Type definitions for our .mokuro file ---
+export interface MokuroBlock {
+  box: [number, number, number, number];
+  lines_coords: [[number, number], [number, number], [number, number], [number, number]][];
+  lines: string[];
+}
+export interface MokuroPage {
+  img_width: number;
+  img_height: number;
+  blocks: MokuroBlock[];
+  img_path: string;
+}
+export interface MokuroData {
+  title: string;
+  title_uuid: string;
+  volume: string;
+  pages: MokuroPage[];
+}
+
+export interface VolumeResponse {
+  id: string;
+  title: string;
+  seriesId: string;
+  pageCount: number;
+  coverImageName: string | null;
+  mokuroData: MokuroData;
+}
