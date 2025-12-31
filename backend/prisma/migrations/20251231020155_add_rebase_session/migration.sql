7a5e37a62e1b6b5a4eeec680403ce9d5840f6596
nguyenston <phucnguyen1999.nn@gmail.com>
2025-12-31 00:14:08 -0500

    feat(backend/prisma): add rebase session table to database
    - This allow the rebase engine to recompute the rebase session state
    when server recover from outage

---
+++ backend/prisma/migrations/20251231020155_add_rebase_session/migration.sql
@@ -0,0 +1,46 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isFloating` on the `OcrBranch` table. All the data in the column will be lost.
+
+*/
+-- CreateTable
+CREATE TABLE "RebaseSession" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "branchId" TEXT NOT NULL,
+    "targetHeadPatchId" TEXT NOT NULL,
+    "headPatchIdSnapshot" TEXT NOT NULL,
+    "resolutions" TEXT NOT NULL DEFAULT '[]',
+    "updatedAt" DATETIME NOT NULL,
+    "expiresAt" DATETIME NOT NULL,
+    CONSTRAINT "RebaseSession_branchId_fkey" FOREIGN KEY ("branchId") REFERENCES "OcrBranch" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "RebaseSession_targetHeadPatchId_fkey" FOREIGN KEY ("targetHeadPatchId") REFERENCES "Patch" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "RebaseSession_headPatchIdSnapshot_fkey" FOREIGN KEY ("headPatchIdSnapshot") REFERENCES "Patch" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_OcrBranch" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "volumeId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "headPatchId" TEXT NOT NULL,
+    "rootPatchId" TEXT,
+    "snapshotPatchId" TEXT,
+    "version" INTEGER NOT NULL DEFAULT 0,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "OcrBranch_headPatchId_fkey" FOREIGN KEY ("headPatchId") REFERENCES "Patch" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "OcrBranch_rootPatchId_fkey" FOREIGN KEY ("rootPatchId") REFERENCES "Patch" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "OcrBranch_volumeId_fkey" FOREIGN KEY ("volumeId") REFERENCES "Volume" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "OcrBranch_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_OcrBranch" ("headPatchId", "id", "rootPatchId", "snapshotPatchId", "updatedAt", "userId", "version", "volumeId") SELECT "headPatchId", "id", "rootPatchId", "snapshotPatchId", "updatedAt", "userId", "version", "volumeId" FROM "OcrBranch";
+DROP TABLE "OcrBranch";
+ALTER TABLE "new_OcrBranch" RENAME TO "OcrBranch";
+CREATE UNIQUE INDEX "OcrBranch_volumeId_userId_key" ON "OcrBranch"("volumeId", "userId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RebaseSession_branchId_key" ON "RebaseSession"("branchId");
